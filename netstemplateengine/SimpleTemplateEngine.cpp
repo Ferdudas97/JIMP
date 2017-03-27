1e547c9ab22eb26975af726058efb34c814e524a
Radek Chrzanowski <ferdudas97@gmail.com>
2017-03-27 20:51:09 +0200

    Add files via upload

--- netstemplateengine/SimpleTemplateEngine.cpp
+++ netstemplateengine/SimpleTemplateEngine.cpp
@@ -12,24 +12,28 @@ nets::View::View(string txt) {
 std::string nets::View::Render(const std::unordered_map<std::string, std::string> &model) const {
 
     string txt=text;
+    int i=0,x=0;
     for (auto &mod: model) {
-       // string tmp=mod.first;
-       // string tmp2=mod.second;
-        cout<<txt<<endl;
-       // cout<<txt.find("{{" + tmp = "}}")<<endl;
-        while (txt.find("{{" + mod.first + "}}") < txt.length()) {
-           // cout<<txt.find("{{" + tmp = "}}")<<"tutaj"<<endl;
-            //txt.replace(txt.find("{{" + tmp + "}}"), tmp.size() + 4, tmp2);
+       if (txt.find("{{" + mod.first + "}}") < txt.length()){
             txt.replace(txt.find("{{" + mod.first +"}}"),mod.first.size()+4,mod.second);
+       }
+    }
+    for (auto &mod: model) {
+        i++;
+       
+        if (i>model.size())break;
+        while (txt.find("{{" + mod.first + "}}") < txt.length() and text.find(mod.second)>=txt.find("{{" + mod.first + "}}")) {
+
+            txt.replace(txt.find("{{" + mod.first + "}}"), mod.first.size() + 4, mod.second);
         }
     }
-    while (txt.find("{{")<txt.size() and txt.find("}}")<txt.size() and txt.find("}}")<txt.find("} "))
-    {
-        if (txt.find("}}")<txt.size() and txt.find("}}")<txt.find("} "))
-        {
-            txt.replace(txt.find("{{"),txt.find("}}")-txt.find("{{")+2,"");
+
+
+
+   for (auto &mod: model) {
+            if (txt.find("{{") < txt.size() and txt.find("}}") < txt.size() and txt.find("}}") < txt.find("} ") and (txt.find(mod.second)>=txt.length() or txt.find("{{" + mod.first + "}}")>=txt.length())){
+                txt.replace(txt.find("{{"), txt.find("}}") - txt.find("{{") + 2, "");
+            }
         }
-    }
-    //cout<<txt<<"teraz ja"<<endl;
     return  txt;
 }
