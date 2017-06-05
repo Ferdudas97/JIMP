e94c57cab9ab17532d6e81456ac7321662e9976b
Radek Chrzanowski <ferdudas97@gmail.com>
2017-06-05 22:34:42 +0200

    Add files via upload

---
+++ timerecorder/TimeRecorder.h
@@ -0,0 +1,28 @@
+//
+// Created by Radek on 05.06.2017.
+//
+
+#ifndef JIMP_EXERCISES_TIMERECORDER_H
+#define JIMP_EXERCISES_TIMERECORDER_H
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+namespace profiling
+{
+    template<class T>
+
+    auto TimeRecorder(T foo)
+    {
+        chrono::time_point<chrono::system_clock> start, end;
+        start = chrono::system_clock::now();
+        decltype(foo()) var = foo();
+        end = chrono::system_clock::now();
+
+        chrono::duration<double, ratio<1, 1000>> time = (end - start);
+
+        return pair<decltype(foo()), double> {var, int(time.count())+1};
+
+    };
+}
+#endif //JIMP_EXERCISES_TIMERECORDER_H
